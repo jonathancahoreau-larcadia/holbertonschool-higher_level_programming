ffeb8a5624bf74aa3cc18bdda3882ebbc0919216
Jonathan <jonathan.cahoreau@gmail.com>
2026-06-08 11:21:14 +0200

    send to github

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Fetch and print all State objects from a MySQL database.
+
+This script connects to a MySQL database using SQLAlchemy and prints all
+records from the `states` table in the format `id: name`, ordered by
+state id.
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}"
+        .format(username, password, database),
+        pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+    session.close()
