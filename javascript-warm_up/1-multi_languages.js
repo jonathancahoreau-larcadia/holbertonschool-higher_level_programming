8019960ce76049f71e858772b2483e845919668f
Jonathan <jonathan.cahoreau@gmail.com>
2026-07-01 15:35:17 +0200

    fix camelCase

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-// Define three messages but note these lines incorrectly assign values to console.log
-const the_first_line = 'C is fun';
-const the_second_line = 'Python is cool';
-const the_third_line = 'JavaScript is amazing';
+// Define three messages
+const theFirstLine = 'C is fun';
+const theSecondLine = 'Python is cool';
+const theThirdLine = 'JavaScript is amazing';
 
-console.log(the_first_line);
-console.log(the_second_line);
-console.log(the_third_line);
+console.log(theFirstLine);
+console.log(theSecondLine);
+console.log(theThirdLine);
