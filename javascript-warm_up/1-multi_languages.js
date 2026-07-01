60467aad3a0fdb2b78c88bfe8e57fc7f77fa9ee9
Jonathan <jonathan.cahoreau@gmail.com>
2026-07-01 15:32:56 +0200

    add multi_languages

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// Define three messages but note these lines incorrectly assign values to console.log
+const the_first_line = 'C is fun';
+const the_second_line = 'Python is cool';
+const the_third_line = 'JavaScript is amazing';
+
+console.log(the_first_line);
+console.log(the_second_line);
+console.log(the_third_line);
